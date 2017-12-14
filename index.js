965977a144ec2ad33f4cc49f6195cce92e1e2918
Sebastian Balling <coolchip@gmx.de>
2017-12-14 23:05:25 +0100

    add function code

--- index.js
+++ index.js
@@ -3,16 +3,21 @@
 const Viera = require('viera.js');
 
 module.exports = function (RED) {
-
     function VieraReadNode(config) {
         RED.nodes.createNode(this, config);
         var node = this;
         var tv = new Viera(config.host);
 
         node.on('input', function (msg) {
-            tv[config.function](function (data) {
-                node.send({payload:data});
-            });
+            var functionName = config.function || msg.function;
+            if (typeof (functionName) !== 'undefined') {
+                return tv[config.function](function (data) {
+                    node.send({
+                        payload: data
+                    });
+                });
+            }
+            RED.log.error(`Function ${functionName} not defined.`);
         });
     }
     RED.nodes.registerType("viera read", VieraReadNode);
@@ -21,15 +26,13 @@ module.exports = function (RED) {
         RED.nodes.createNode(this, config);
         var node = this;
         var tv = new Viera(config.host);
-     
-        node.on('input', function (msg) {
-            var parameterName = config.parameter || msg.parameter;
 
-            //tv.sendCommand("info");
-            //tv.setVolume(19);
-            //tv.setMute(true);
-            tv[config.function](msg.payload);
-            //node.send(msg);
+        node.on('input', function (msg) {
+            var functionName = config.function || msg.function;
+            if (typeof (functionName) !== 'undefined') {
+                return tv[config.function](msg.payload);
+            }
+            RED.log.error(`Function ${functionName} not defined.`);
         });
     }
     RED.nodes.registerType("viera write", VieraWriteNode);
