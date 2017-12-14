bc87edd2c7940d7f2d44f7c5a7f10fab95710f4d
Sebastian Balling <coolchip@gmx.de>
2017-12-14 22:15:47 +0100

    init

---
+++ index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const Viera = require('viera.js');
+
+module.exports = function (RED) {
+
+    function VieraReadNode(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+        var tv = new Viera(config.host);
+
+        node.on('input', function (msg) {
+            tv.getVolume(function (data) {
+                node.send({payload:data});
+            });
+        });
+    }
+    RED.nodes.registerType("viera read", VieraReadNode);
+
+    function VieraWriteNode(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+        var tv = new Viera(config.host);
+     
+        node.on('input', function (msg) {
+            var parameterName = config.parameter || msg.parameter;
+
+            tv.sendCommand("info");
+            //tv.setVolume(19);
+            //tv.setMute(true);
+            //tv.sendCommand(parameterName)
+            //node.send(msg);
+        });
+    }
+    RED.nodes.registerType("viera write", VieraWriteNode);
+}
